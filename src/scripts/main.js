b3239875b31ab8639fcb4709ee16d5f54dae66fc
Kaiky Damasio <kaikydamasio5@gmail.com>
2024-07-08 23:26:44 -0300

    update2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2024 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento= timeStampDoEvento - timeStampAtual;
+
+    const diasEmMs = 1000 * 60 * 60 * 24;
+    const HoraEmMs = 1000 * 60 * 60;
+    const MinutoEmMs= 1000 * 60;
+    const segundoEmMS = 1000;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diasEmMs) / HoraEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % HoraEmMs) / MinutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % MinutoEmMs) / segundoEmMS);
+
+    document.getElementById('contador').innerHTML= `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+},1000)
